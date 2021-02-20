96e9dd92af9033c22d37ce94ca9c622ff0ea34ad
jonathan-casteloot <79249687+jonathan-casteloot@users.noreply.github.com>
2021-02-20 20:39:09 +0100

    Add first step for creating a repo

--- from_local_to_server.sh
+++ from_local_to_server.sh
@@ -1,6 +1,6 @@
+# first step : creation a repo on Github
 git init
 git add .
 git commit -m "Create First Commit"
 git remote add origin git@github.com:jonathan-casteloot/git_inception.git
-git branch --set-upstream-to=origin/master master
-git pull origin master --allow-unrelated-histories
+git push origin master
