5577ea2a4f5edab930fd784936c3ae91c0036ddc
jonathan-casteloot <jonathan.casteloot@gmail.com>
2021-02-20 21:50:09 +0100

    Update branches creation script

--- create_branches_model.sh
+++ create_branches_model.sh
@@ -2,5 +2,5 @@ git checkout -b develop
 git branch feature_1
 git branch feature_2
 git push origin develop
-git push feature_1
-git push feature_2
+git push origin feature_1
+git push origin feature_2
